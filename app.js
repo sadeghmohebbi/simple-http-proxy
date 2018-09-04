3418970a62effb7b0797adee4700158928bf5717
sadegh mohebbi <mohebbi.sadegh@gmail.com>
2018-09-04 22:33:42 +0430

    http server removed

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-const http = require('http')
-const ProxyChain = require('proxy-chain');
-
-const requestHandler = (request, response) => {
-    response.end('Hello Node.js Http Proxy Server!')
-}
-
-const http_server = http.createServer(requestHandler)
-
-let http_port = process.env.PORT || 80;
-http_server.listen(http_port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`Http server is listening on port ${http_port}`);
-})
+const ProxyChain = require('proxy-chain')
 
 let proxy_port = process.env.PORT || 8000;
 const proxy_server = new ProxyChain.Server({
