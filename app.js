2c8d3907ee215d90ba74aa6d7d3f5838783ecdae
sadegh mohebbi <mohebbi.sadegh@gmail.com>
2018-09-04 21:58:28 +0430

    app js server deployed

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+const http = require('http')
 const ProxyChain = require('proxy-chain');
+
+const requestHandler = (request, response) => {
+    response.end('Hello Node.js Http Proxy Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(8080, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+})
+
 const server = new ProxyChain.Server({
     port: 8000,
     verbose: false,
     prepareRequestFunction: ({ request, username, password, hostname, port, isHttp }) => {
         return {
-            requestAuthentication: username !== 'simplehttpproxy' || password !== 'simplehttpproxy'
+                requestAuthentication: username !== 'sadeghmohebbi' || password !== '123456789'
         };
     },
-});
+})
 
 server.listen(() => {
     console.log(`Proxy server is listening on port ${8000}`);
-});
+})
