e301c1f794641a060fd371d26df982d0dea3c0a7
sadegh mohebbi <mohebbi.sadegh@gmail.com>
2018-09-04 22:29:42 +0430

    port logs deployed

--- app.js
+++ app.js
@@ -7,23 +7,25 @@ const requestHandler = (request, response) => {
 
 const http_server = http.createServer(requestHandler)
 
-http_server.listen(process.env.PORT || 80, (err) => {
+let http_port = process.env.PORT || 80;
+http_server.listen(http_port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
-    console.log(`Http server is listening on port ${8080}`);
+    console.log(`Http server is listening on port ${http_port}`);
 })
 
+let proxy_port = process.env.PORT || 8000;
 const proxy_server = new ProxyChain.Server({
-    port: process.env.PORT || 8000,
+    port: proxy_port,
     verbose: false,
     prepareRequestFunction: ({ request, username, password, hostname, port, isHttp }) => {
         return {
-                requestAuthentication: username !== 'sadeghmohebbi' || password !== '123456789'
-        };
+            requestAuthentication: username !== 'sadeghmohebbi' || password !== '123456789'
+        }
     },
 })
 
 proxy_server.listen(() => {
-    console.log(`Proxy server is listening on port ${8000}`);
+    console.log(`Proxy server is listening on port ${proxy_port}`);
 })
